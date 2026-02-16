65f5f2721ed595ffe11da0ed10aba3cc6605b198
Tim-ru <httimoha176@gmail.com>
2026-02-16 11:26:23 +0300

    fix

--- Assets/_Project/Scripts/Gameplay/Player/PlayerController.cs
+++ Assets/_Project/Scripts/Gameplay/Player/PlayerController.cs
@@ -1,28 +1,25 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
+
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 5f;
     [SerializeField] private Rigidbody2D rb;
 
     private Vector2 moveInput;
-
     void Awake()
     {
         if (rb == null) rb = GetComponent<Rigidbody2D>();
     }
 
-    public void OnMove(InputValue value)
+    public void OnMove(Vector2 move)
     {
-        moveInput = value.Get<Vector2>();
-
-        Debug.Log("OnMove called: " + moveInput);
+        moveInput = move;
     }
 
     void FixedUpdate()
     {
         rb.linearVelocity = new Vector2(moveInput.x * moveSpeed, rb.linearVelocity.y);
-        
+
         if (moveInput.x != 0)
         {
             Debug.Log(moveInput.x);
---
+++ Assets/_Project/Scripts/Gameplay/Player/PlayerInputHandler.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInputHandler : MonoBehaviour
+{
+    [SerializeField] private PlayerController playerController;
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        Vector2 move = context.ReadValue<Vector2>();
+        Debug.Log($"OnMove called, value: {move}");
+
+        if (playerController != null)
+        {
+            Debug.Log($"OnMove called, controller is null: {playerController == null}, value: {context.ReadValue<Vector2>()}");
+            playerController.OnMove(move);
+        }
+    }
+}
