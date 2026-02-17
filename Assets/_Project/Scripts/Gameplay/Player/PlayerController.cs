d161c3ba1c524f8b6ba5d3937ef71bec8a24b81e
Checoffix <vnikita.232003@gmail.com>
2026-02-17 19:27:17 +0700

    Deleted extra debug info

--- Assets/_Project/Scripts/Gameplay/Player/PlayerController.cs
+++ Assets/_Project/Scripts/Gameplay/Player/PlayerController.cs
@@ -101,7 +101,6 @@ public class PlayerController : MonoBehaviour
         direction = (int)move.x;
         if (direction != 0) spriteRenderer.flipX = direction < 0;
         animator.SetFloat(xVelocity, Math.Abs(move.x));
-        Debug.Log(move.x);
         moveInput = move;
     }
 
