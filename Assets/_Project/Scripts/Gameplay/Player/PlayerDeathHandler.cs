b835f469e564bc68aaf4c2f4b6fdaca154e40f35
Tim-ru <httimoha176@gmail.com>
2026-02-18 14:53:27 +0300

    feat: add die handler

--- Assets/_Project/Scripts/Gameplay/Player/PlayerDeathHandler.cs
+++ Assets/_Project/Scripts/Gameplay/Player/PlayerDeathHandler.cs
@@ -1,16 +1,43 @@
+using System.Collections;
 using UnityEngine;
 
 public class PlayerDeathHandler : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private Transform respawnPoint;
+    [SerializeField] private Animator fadeAnimator;
+    [SerializeField] private float deathDelayBeforeRespawn = 0.5f;
+
+    private PlayerController playerController;
+    private Rigidbody2D rb;
+    private bool isDead;
+
+    private void Awake()
     {
-        
+        playerController = GetComponent<PlayerController>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void HandleDeathTrigger(GameObject other, GameObject trigger)
     {
-        
+        if (other != gameObject) return;
+        Die();
+    }
+
+    public void Die()
+    {
+        if (isDead) return;
+        isDead = true;
+        if (playerController != null) playerController.enabled = false;
+        if (fadeAnimator != null) fadeAnimator.SetTrigger("Die");
+        StartCoroutine(DeathRoutine());
+    }
+
+    private IEnumerator DeathRoutine()
+    {
+        yield return new WaitForSeconds(deathDelayBeforeRespawn);
+        transform.position = respawnPoint != null ? respawnPoint.position : transform.position;
+        if (rb != null) rb.linearVelocity = Vector2.zero;
+        isDead = false;
+        if (playerController != null) playerController.enabled = true;
     }
 }
